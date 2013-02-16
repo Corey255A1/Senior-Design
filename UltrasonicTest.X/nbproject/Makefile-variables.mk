8e1c3d652d27e4038b6a9ce796859b37b08c5cef
Daniel Pramik <dwp11@zips.uakron.edu>
2013-02-16 11:39:16 -0500

    Ultrasonic test Code
    Code for hagisonic ultrasonics

---
+++ UltrasonicTest.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=UltrasonicTest.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/UltrasonicTest.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ultrasonictest.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ultrasonictest.x.tar
---
+++ UltrasonicTest.X/ultrasonic.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   ultrasonic.c
+ * Author: Dan
+ *
+ * Created on February 14, 2013, 6:29 PM
+ */
+
+#include <p24ep32mc202.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+
+    return (EXIT_SUCCESS);
+}
+
