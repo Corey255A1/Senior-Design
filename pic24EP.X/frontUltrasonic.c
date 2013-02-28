9ef25af7da8c77f278bc3a8b81cb7c6088a4dc1c
Daniel Pramik <dwp11@zips.uakron.edu>
2013-02-28 18:36:11 -0500

    added empty frontUltrasonic file

---
+++ pic24EP.X/frontUltrasonic.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   frontUltrasonic.c
+ * Author: Dan
+ *
+ * Created on February 28, 2013, 6:35 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
