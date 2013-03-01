ad13e2eaa983f7abed31d34e11c3c64e31a67ba0
Nicholas Fragiskatos <nf14@zips.uakron.edu>
2013-03-01 16:05:15 -0500

    Added SPI files
    Added the SPI files to the drive control

---
+++ SlaveDriveControl.X/spi.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   spi.h
+ * Author: student
+ *
+ * Created on February 28, 2013, 6:26 PM
+ */
+/**
+ * Configure the Serial Peripheral Interface (SPI) module of the PIC to act as
+ * a slave.
+ *
+ * @return void
+ */
+void configSPICommunication(void);
+extern int SPI_msg;
+extern int SPI_msg_rdy;
+
