f605fa9ce5fa2a42ec1116cff61dc6bdfc41ed21
wesd3fer <nf14@zips.uakron.edu>
2013-04-04 15:11:41 -0400

    IDK

--- SlaveDriveControl.X/SlaveDriveControl.c
+++ SlaveDriveControl.X/SlaveDriveControl.c
@@ -124,21 +124,21 @@ int main(void) {
     //char distance;
 
     DRIVE_EN        = EN;
-    SPEEDM1         = 100;
-    SPEEDM2         = 100;
+    //SPEEDM1         = 100;
+    //SPEEDM2         = 100;
     msgQueued       = EN;
-    M1FWD           = 1;
-    M1REV           = 0;
-    M2FWD           = 1;
-    M2REV           = 0;
+    //M1FWD           = 1;
+    //M1REV           = 0;
+    //M2FWD           = 1;
+    //M2REV           = 0;
   
     while(1)
     {
         //---------------------------------------------------------------------
         //  If a message needs decoding...
         //---------------------------------------------------------------------
-        //if (curMasterConfig != INMSG)
-        if (SPEEDM1 == 20)
+        if (curMasterConfig != INMSG)
+        //if (SPEEDM1 == 20)
         {
             curMasterConfig = INMSG;
             M1FdBckB_SampCount = 0;
